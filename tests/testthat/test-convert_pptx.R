192e21b230f691b2fb0b861583e41879a242a604
datalorax <daniela@uoregon.edu>
2018-06-08 22:32:39 -0700

    Another shot in the dark to try to get travis to run

--- tests/testthat/test-convert_pptx.R
+++ tests/testthat/test-convert_pptx.R
@@ -1,8 +1,10 @@
 context("test-convert_pptx.R")
-path <- system.file("examples", "slidedemo.pptx", package = "slidex")
-out <- convert_pptx(path, "Daniel")
+
 
 test_that("Basic files are where they're supposed to be", {
+  path <- system.file("examples", "slidedemo.pptx", package = "slidex")
+  out <- convert_pptx(path, "Daniel")
+
   expect_equal(file.exists("slidedemo.Rmd"), TRUE)
   expect_equal(dir.exists("assets"), TRUE)
   expect_equal(dir.exists("slidedemo_xml"), FALSE)
@@ -17,6 +19,8 @@ unlink("assets", recursive = TRUE)
 unlink(rmd, recursive = TRUE)
 
 test_that("Error is thrown when file is not pptx", {
+  path <- system.file("examples", "slidedemo.pptx", package = "slidex")
+
   expect_error(convert_pptx(substr(path, 1, nchar(path) - 1), "Daniel"))
   expect_equal(dir.exists("slidedemo_xml"), FALSE)
   expect_equal(file.exists("slidedemo.Rmd"), FALSE)
